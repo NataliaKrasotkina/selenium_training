fdf43d5cec7d41250bb1dcf6ddc8a01db9eab923
NataliaKrasotkina <NataliaKrasotkina@coherentsolutions.com>
2023-05-29 12:32:10 +0300

    Task_30 - introduction to selenium web driver

--- src/main/java/pages/YandexLoginPage.java
+++ src/main/java/pages/YandexLoginPage.java
@@ -3,7 +3,7 @@ package pages;
 import org.openqa.selenium.By;
 
 public class YandexLoginPage {
-    public final static By ENTER_BUTTON = By.cssSelector(".Button2.Button2_type_link.Button2_view_default.Button2_size_m");
+    public final static By ENTER_BUTTON = By.cssSelector(".Button2_view_default.Button2_size_m");
     public final static By LOGIN_INPUT = By.id("passp-field-login");
     public final static By LOGIN_BUTTON = By.id("passp:sign-in");
     public final static By PASSWORD_INPUT = By.name("passwd");
