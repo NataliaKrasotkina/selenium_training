33753a04d15339abc5661c18cb3931cdd972741f
NataliaKrasotkina <NataliaKrasotkina@coherentsolutions.com>
2023-11-22 11:50:20 +0200

    allure

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,10 +1,15 @@
+import io.qameta.allure.Description;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Story;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import pages.LoginPage;
 import pages.MailPage;
 
-
+@Epic("Login Tests Epic")
+@Feature("Invalid Login Features")
 public class LoginTest extends BaseTest {
     private final static String PASSWORD = "NK852456";
     private final static String LOGIN = "NatalliaKrasotkina";
@@ -19,6 +24,8 @@ public class LoginTest extends BaseTest {
     }
 
     @Test
+    @Story("User tries to login the system with invalid username and invalid password.")
+    @Description("Invalid Login Test with Invalid Username and Invalid Password.")
     void loginTest() {
 
         Assertions.assertEquals(LOGIN, mailPage.getAccountName(), "Login failed");
